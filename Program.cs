e7452d0526ee999c64e10628baba1eac2d785813
Jaiveer2525 <jayeshbisht25@gmail.com>
2025-06-26 10:04:09 +0000

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class HelloWorlder
+    {
+        static void Main(String[] args){
+            if (args.Length > 0 && args[0] == "-s"){}
+            else{
+                Console.WriteLine("Hello World");
+            }
+        }
+    }
+}
