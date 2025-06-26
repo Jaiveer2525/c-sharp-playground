39a8e5746d04281058ff26223b32615abcee6585
Jaiveer2525 <jayeshbisht25@gmail.com>
2025-06-26 11:01:25 +0000

    Shifted to a CLI ToDo

--- Program.cs
+++ Program.cs
@@ -1,14 +1,51 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Data.Common;
 
 namespace HelloWorld
 {
-    class HelloWorlder
+    class TaskItem
     {
-        static void Main(String[] args){
-            if (args.Length > 0 && args[0] == "-s"){}
-            else{
-                Console.WriteLine("Hello World");
+        public int Id {get; set;}
+        public string Desc {get; set;} = "New Empty Task";
+        public bool IsDone {get; set;} = false;
+    }
+
+    class TaskManager
+    {
+        public TaskManager()
+        {
+            read();
+        }
+
+        private const string fileName = "todo.json";
+        private List<TaskItem> tasks = new();
+
+        int id;
+        public void add (string desc){
+            id = tasks.Count != 0 ? tasks.Max(tasks => tasks.Id) : 1;
+            tasks.Add(new TaskItem {Id = id , Desc = desc});
+            write();
+        }
+
+        public void done () {}
+        public void list () {
+            if (tasks.Count == 0){
+                Console.WriteLine("The ToDo is empty!");
+            }else
+            {
+                Console.WriteLine("Id\tIsDone\tDescription");
+                foreach (var t in tasks){
+                    Console.WriteLine(t.Id + "\t" + t.IsDone + "\t" + t.Desc);
+                } 
             }
         }
+        public void delete (int id) {
+            tasks.RemoveAll(t => t.Id == id);
+            write();
+        }
+
+        private void read () {}
+        private void write () {}
     }
 }
